8cadd01fb322e4613ae7278d42b657e97066b361
Adilius <leugue-gamer@hotmail.com>
2021-04-22 17:44:37 +0200

    Updated volume visualization code

--- main.py
+++ main.py
@@ -17,25 +17,23 @@ reader.SetDataExtent(0, 255, 0, 255, 0, 177)
 reader.SetDataScalarTypeToUnsignedChar()
 reader.Update()
 
+#Colors
+colorTransferFunction = vtk.vtkColorTransferFunction()
+colorTransferFunction.SetColorSpaceToRGB()  #Useless?
+colorTransferFunction.AddRGBPoint(0.0, 0.0, 0.0, 0.0)   #First point, all black
+colorTransferFunction.AddRGBPoint(86.0, 1.0, 0.0, 0.0)  #One third points, all red
+colorTransferFunction.AddRGBPoint(172.0, 0.0, 1.0, 0.0) #Two thirds points, all green
+colorTransferFunction.AddRGBPoint(255.0, 0.0, 0.0, 1.0) #Three thirds points, all blue
+
 #Mapper pushes geomtry into graphics library, color mapping with scalar attributes.
-cubeMapper = vtk.vtkPolyDataMapper()
+cubeMapper = vtk.vtkPolyDataMapper()                    #Creates mapper
 cubeMapper.SetInputConnection(reader.GetOutputPort())   #Clears and specifies a single connection to the port
+cubeMapper.SetLookupTable(colorTransferFunction)        #Sets color lookup table
 
 #Opacity
 opacityTransferFunction = vtk.vtkPiecewiseFunction()
-opacityTransferFunction.AddPoint(20, 0.0)
-opacityTransferFunction.AddPoint(255, 0.2)
-
-#Color
-colorTransferFunction = vtk.vtkColorTransferFunction()
-colorTransferFunction.AddRGBPoint(0.0, 0.0, 0.0, 0.0)
-colorTransferFunction.AddRGBPoint(64.0, 1.0, 0.0, 0.0)
-colorTransferFunction.AddRGBPoint(128.0, 0.0, 0.0, 1.0)
-colorTransferFunction.AddRGBPoint(192.0, 0.0, 1.0, 0.0)
-colorTransferFunction.AddRGBPoint(255.0, 0.0, 0.2, 0.0)
-colorTransferFunction.AddRGBPoint(255.0, 0.0, 0.2, 0.0)
-colorTransferFunction.AddRGBPoint(255.0, 0.2, 0.0, 0.0)
-
+opacityTransferFunction.AddPoint(0, 0.0)    #Add lowest opacity
+opacityTransferFunction.AddPoint(255, 0.4)  #Add highest opacity
 
 #Volume property
 volumeProperty = vtk.vtkVolumeProperty()
@@ -49,6 +47,16 @@ volumeMapper = vtk.vtkGPUVolumeRayCastMapper()
 volumeMapper.SetBlendModeToComposite()
 volumeMapper.SetInputConnection(reader.GetOutputPort())
 
+# Setup color mapping bar
+colorBar = vtk.vtkScalarBarActor()
+colorBar.SetLookupTable(cubeMapper.GetLookupTable())
+colorBar.SetTitle("Colormap")
+colorBar.SetNumberOfLabels(0)
+colorBar.SetLabelFormat("%6.0f")
+colorBar.SetPosition(0.9, 0.1)
+colorBar.SetWidth(0.08)
+colorBar.SetHeight(0.5)
+
 #Set volume
 volume = vtk.vtkVolume()
 volume.SetMapper(volumeMapper)
@@ -57,15 +65,16 @@ volume.SetProperty(volumeProperty)
 #Create renderer
 ren = vtk.vtkRenderer()
 ren.AddVolume(volume)
+ren.AddActor(colorBar)
 
 #Create window
 window = vtk.vtkRenderWindow()
 window.SetSize(1000,1000)
 window.AddRenderer(ren)
 
-#Create interactor
-interactor = vtk.vtkRenderWindowInteractor()
-interactor.SetRenderWindow(window)
+#Create interactor that captures mouse interactions with window
+interactor = vtk.vtkRenderWindowInteractor()    #Create
+interactor.SetRenderWindow(window)              #Add
 
 #Start renderer & interactor
 window.Render()
